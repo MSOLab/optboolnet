982c786ca2364c99afc7a1f5068942c25fd07b31
Loïc Paulevé <loic.pauleve@labri.fr>
2023-11-15 23:28:59 +0100

    model checking for attractors and phenotype

---
+++ src/optboolnet/checking.py
@@ -0,0 +1,112 @@
+
+import os
+import tempfile
+from nusmv import NuSMV
+
+def _nusmv_var(n):
+    if isinstance(n, int):
+        return "x%d" % n
+    return n
+def _nusmv_model(bn, update_mode="synchronous"):
+    """
+    bn: minibn.CNFBooleanNetwork
+    update_mode: synchronous, asynchronous
+    """
+
+    dom = bn.vars_list
+    udom = ["u%s" % n for n in dom]
+    var = _nusmv_var
+
+    lines = ["MODULE main"]
+    lines.append("VAR")
+    for i in dom:
+        lines.append("%s: boolean;" % var(i))
+    lines.append("ASSIGN")
+    for i in dom:
+        if update_mode == "synchronous":
+            lines.append("next(%s) := f%s;" % (var(i),i))
+        else:
+            lines.append("next(%s) := {%s, f%s};" % (var(i),var(i),i))
+
+    lines.append("DEFINE")
+    for n in bn.vars_list:
+        clauses = bn.items_clause(n)
+        if not clauses:
+            lines.append(f"f{n} := FALSE;")
+        elif len(clauses) == 1 and not clauses[0].args:
+            lines.append(f"f{n} := TRUE;")
+        else:
+            def smv_or(clause):
+                neg = [f"!{var(m)}" for m in clause.neg_literals]
+                pos = [f"{var(m)}" for m in clause.pos_literals]
+                expr = " | ".join(neg+pos)
+                if len(neg+pos) > 1:
+                    expr = f"({expr})"
+                return expr
+            smv_and = " & ".join((smv_or(clause) for clause in clauses))
+            lines.append(f"f{n} := {smv_and};")
+
+    if update_mode != "synchronous":
+        lines.append("FIXEDPOINTS := %s;" \
+            % (" & ".join(["%s = f%s" % (var(i),i) for i in dom])))
+        lines.append("TRANS")
+        lines.append("  FIXEDPOINTS")
+        if update_mode == "general":
+            for i in dom:
+                lines.append("| next(%s) != %s" % (var(i),var(i)))
+        elif update_mode == "asynchronous":
+            for i in dom:
+                freeze = " & ".join(["next({0})={0}".format(var(j)) for j in dom if i != j])
+                freeze = " & %s" % freeze if freeze else ""
+                lines.append("| next({0})!={0}{1}".format(var(i),freeze))
+        lines.append(";")
+    return "\n".join(lines) + "\n"
+
+def _nusmv_state(dstate):
+    def _expr(n, v):
+        return f"{'!' if not v else ''}{_nusmv_var(n)}"
+    return " & ".join((_expr(n, v) for n,v in dstate.items()))
+
+def _nusmv_alltrue(nusmv_input, smvfile):
+    tmp_smvfile = smvfile is None
+    if tmp_smvfile:
+        _, smvfile = tempfile.mkstemp(suffix=".smv")
+    try:
+        with open(smvfile, "w") as fp:
+            fp.write(nusmv_input)
+        mc = NuSMV(smvfile)
+        return mc.alltrue()
+    finally:
+        if tmp_smvfile:
+            os.unlink(smvfile)
+
+def nusmv_check_attractor(bn, attractor, update_mode="synchronous",
+                smvfile=None):
+    """
+    Returns true if attractor is indeed an attractor of the bn
+
+    bn: CNFBooleanNetwork
+    attractor: Attractor
+    update_mode: synchronous, asynchronous, general
+    smvfile: if None makes a temporary file to be deleted after call
+    """
+    dstate = dict(zip(sorted(bn.vars_list), attractor.value_list[0]))
+    dstate_smv = _nusmv_state(dstate)
+    nusmv_input = _nusmv_model(bn, update_mode=update_mode)
+    nusmv_input += f"INIT {dstate_smv};\n"
+    nusmv_input += f"CTLSPEC AG EF ({dstate_smv});"
+    return _nusmv_alltrue(nusmv_input, smvfile)
+
+
+def nusmv_check_phenotype(bn, update_mode="synchronous",
+                smvfile=None):
+    """
+    Returns true if all the attractors have p=1 constantly
+
+    bn: CNFBooleanNetwork
+    update_mode: synchronous, asynchronous, general
+    smvfile: if None makes a temporary file to be deleted after call
+    """
+    nusmv_input = _nusmv_model(bn, update_mode=update_mode)
+    nusmv_input += f"CTLSPEC AG EF {bn.phenotype};"
+    return _nusmv_alltrue(nusmv_input, smvfile)
---
+++ tests/test_08_checking.py
@@ -0,0 +1,29 @@
+from optboolnet.instances import load_bn_in_repo
+from optboolnet.model import AttractorDetectionIP
+from optboolnet.config import SolverConfig
+from optboolnet import checking
+
+def test_nusmv_check_attractor():
+    bn = load_bn_in_repo("S1")
+    _solver_config = {
+        "solver_name": "gurobi_persistent",
+        "save_results": False,
+        "tee": False,
+        "warmstart": False,
+        "time_limit": 3.0,
+    }
+    length = 6
+    attr_ip = AttractorDetectionIP("M1", bn, length, SolverConfig(_solver_config))
+    attr_ip.make_constr_stability_condition()
+    attr_ip.set_constr_target_size(0)
+    attr_ip.optimize()
+    attractor = attr_ip.get_attractor()
+    assert checking.nusmv_check_attractor(bn, attractor)
+
+def test_nusmv_check_phenotype():
+    bn = load_bn_in_repo("S1")
+    assert not checking.nusmv_check_phenotype(bn)
+
+if __name__ == "__main__":
+    test_nusmv_check_attractor()
+    test_nusmv_check_phenotype()
