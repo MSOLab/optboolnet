8c4c64240e10af4b99f4f534bf77577508c73041
kalebmoon07 <kaleb.moon@postech.ac.kr>
2025-09-23 02:17:32 +0900

    Updated conda packaging options

--- .scripts/run_conda_build.sh
+++ .scripts/run_conda_build.sh
@@ -2,4 +2,4 @@
 Build=$(python ./src/optboolnet/version.py name)
 cwd=$(pwd)
 echo Building $Build for Anaconda.org
-conda build "./recipe" --output-folder "./dist/conda" -c colomoto -c gurobi -c conda-forge
+conda-build "./recipe" --output-folder "./dist/conda" -c colomoto -c gurobi -c conda-forge
--- .scripts/upload_conda.bat
+++ .scripts/upload_conda.bat
@@ -2,4 +2,4 @@ python .\src\optboolnet\version.py name > temp_output
 set /p Build=<temp_output
 DEL temp_output
 echo "Uploading %Build% to Anaconda.org"
-anaconda upload %cd%\dist\conda\noarch\%Build%-py_0.tar.bz2
+anaconda upload %cd%\dist\conda\noarch\%Build%-py_0.conda
