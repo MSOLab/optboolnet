34915383aaf79025635086b391e75cf7f1633361
kalebmoon07 <kaleb.moon@postech.ac.kr>
2025-09-23 18:06:16 +0900

    script to upload Anaconda group msolab

--- .scripts/upload_conda.sh
+++ .scripts/upload_conda.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+# Upload the conda package to Anaconda.org
+# Usage: ./upload_conda.sh {personal|group}
 Build=$(python ./src/optboolnet/version.py name)
 cwd=$(pwd)
 echo Uploading $Build to Anaconda.org
-anaconda upload $cwd/dist/conda/noarch/$Build-py_0.conda
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 {personal|group}"
+    exit 1
+fi
+
+CHANNEL=$1
+case "$CHANNEL" in
+    personal)
+        echo "Uploading opboolnet to your personal Anaconda.org account"
+        anaconda upload $cwd/dist/conda/noarch/$Build-py_0.conda
+    ;;
+    group)
+        echo "Uploading opboolnet to Anaconda.org group 'msolab'"
+        anaconda upload --user msolab $cwd/dist/conda/noarch/$Build-py_0.conda
+    ;;
+    *)
+        echo "Unknown option: $CHANNEL (must be 'personal' or 'group')"
+        exit 1
+    ;;
+esac
