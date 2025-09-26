3bd14233811305cb7d34afbaf6501081b8826a9d
kalebmoon07 <kaleb.moon@postech.ac.kr>
2025-09-26 23:10:36 +0900

    Updated conda scripts

--- .scripts/run_conda_build.sh
+++ .scripts/run_conda_build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 Build=$(python ./src/optboolnet/version.py name)
 cwd=$(pwd)
+
 echo Building $Build for Anaconda.org
-conda-build "./recipe" --output-folder "./dist/conda" -c colomoto -c gurobi -c conda-forge
+
+# default: run tests
+TEST_ARGS=""
+if [[ "$1" == "--skip-test" ]]; then
+  echo "Skipping tests during conda-build"
+  TEST_ARGS="--no-test"
+fi
+
+conda-build "./recipe" \
+  --output-folder "./dist/conda" \
+  -c colomoto -c gurobi -c conda-forge \
+  $TEST_ARGS
