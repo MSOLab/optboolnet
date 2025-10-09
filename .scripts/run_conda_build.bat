6830583caa0700d0a0a86dccbf3981aa317a179f
Kyungduk Moon <kaleb.moon@postech.ac.kr>
2025-10-09 15:08:21 +0900

    Version upgrade to 1.0.1

--- .scripts/run_conda_build.bat
+++ .scripts/run_conda_build.bat
@@ -1,6 +1,15 @@
 if not exist ".\dist" md ".\dist"
 python .\src\optboolnet\version.py name > temp_output
 set /p Build=<temp_output
-DEL temp_output
-echo "Building %Build% for Anaconda.org"
-conda-build ".\recipe" --output-folder ".\dist\conda" -c colomoto -c gurobi -c conda-forge
+del temp_output
+
+echo Building %Build% for Anaconda.org
+
+rem default: run tests
+set "TEST_ARGS="
+if /i "%~1"=="--skip-test" (
+    echo Skipping tests during conda-build
+    set "TEST_ARGS=--no-test"
+)
+
+conda-build ".\recipe" --output-folder ".\dist\conda" -c colomoto -c gurobi -c conda-forge %TEST_ARGS%
--- .scripts/upload_conda.bat
+++ .scripts/upload_conda.bat
@@ -1,5 +1,28 @@
-python .\src\optboolnet\version.py name > temp_output
-set /p Build=<temp_output
-DEL temp_output
-echo "Uploading %Build% to Anaconda.org"
-anaconda upload %cd%\dist\conda\noarch\%Build%-py_0.conda
+@echo off
+setlocal
+
+rem Get build name
+for /f "usebackq delims=" %%i in (`python .\src\optboolnet\version.py name`) do set "Build=%%i"
+set "cwd=%cd%"
+
+echo Uploading %Build% to Anaconda.org
+
+if "%~1"=="" (
+    echo Usage: %~nx0 {personal^|group}
+    exit /b 1
+)
+
+set "CHANNEL=%~1"
+
+if /i "%CHANNEL%"=="personal" (
+    echo Uploading optboolnet to your personal Anaconda.org account
+    anaconda upload "%cwd%\dist\conda\noarch\%Build%-py_0.conda"
+) else if /i "%CHANNEL%"=="group" (
+    echo Uploading optboolnet to Anaconda.org group 'msolab'
+    anaconda upload --user msolab "%cwd%\dist\conda\noarch\%Build%-py_0.conda"
+) else (
+    echo Unknown option: %CHANNEL% (must be 'personal' or 'group')
+    exit /b 1
+)
+
+endlocal
